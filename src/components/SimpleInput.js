7bd9afc3f9de6de2009a8a1b61f2c891da0a7636
OEM Configuration (temporary user) <giorgicxvedadze4@gmail.com>
2022-06-25 23:25:05 +0400

    modified

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 
 const SimpleInput = (props) => {
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
   // we can just read about that inforation which user entered in input 
   const enteredInputRef = useRef();
 
   const formSubmission = (event) => {
     event.preventDefault();
+
+    if(enteredInputRef.current.value.trim() === ''){
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
 
     // we need this current value because this input element now is an object and it has every property
     // such as objact has , and we need to reach them and store or send this infomation everywhere we want 
@@ -29,6 +38,7 @@ const SimpleInput = (props) => {
           id='name'
           ref={enteredInputRef} //we need this ref key-word to point useRef and make this input element real Dom Object
         />
+        {!enteredNameIsValid && <p className="error-text">Input must not be an empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
