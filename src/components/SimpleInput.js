b188b0b7227ea6b51cf0f9a83d86633a02eba4da
OEM Configuration (temporary user) <giorgicxvedadze4@gmail.com>
2022-06-25 19:53:25 +0400

    listening to the user input and submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,36 @@
+import { useState } from "react";
+
+
+
 const SimpleInput = (props) => {
+  const [enteredname, setEnteredName] = useState('');
+
+  // listening on every key-stroke and set the update state 
+  const inputCHangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = (event) => {
+    // prevent forms default behavior , which is to 
+    // the send request when form is submitted
+    event.preventDefault();
+
+    console.log(enteredname);
+
+    // set input to the empty string when form is already submitted 
+    setEnteredName('');
+  }  
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={inputCHangeHandler} //listening to every key-stroke
+          value={enteredname}  // set value useState's arrays first meaning after form submitted 
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
